0c593d0b58fa4f0a90f1dc120ae84f207619f47c
David Watts <djowatts@googlemail.com>
2017-10-11 15:26:18 +0100

    Xunit project (#2)
    * Make work with xunit. Remove inconclusive assert. Tests should pass or fail
    
    * Revert "Make work with xunit. Remove inconclusive assert. Tests should pass or fail"
    
    This reverts commit 8187917c1a4f577280f6ad5b807cf6ee8aae8eba.
    
    * Ignoring visual studio files
    
    * Xunit project

---
+++ src/AsyncAssert.Xunit/AsyncAssert.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using Xunit;
+
+namespace AsyncAssert.Xunit
+{
+    public static class AsyncAssert
+    {
+        private static IAsyncAssertLogger _logger;
+
+        public static IAsyncAssertLogger Logger
+        {
+            get => _logger ?? new NullAsyncAssertLogger();
+            set => _logger = value;
+        }
+
+        public static void TrueWithin(Func<bool> function, TimeSpan within, Func<string> getFailMsg)
+        {
+            TrueWithin(function, within, (TimeSpan?)null, getFailMsg);
+        }
+
+        public static void TrueWithin(Func<bool> func, TimeSpan timeSpan, Func<bool> inconclusive, Func<string> inconclusiveMsg, IList<Action> failureActions = null)
+        {
+            TrueWithin(func, timeSpan, (TimeSpan?)null, (Func<string>)null, failureActions);
+        }
+
+        public static void TrueWithin(Func<bool> function, TimeSpan within, TimeSpan? interval = null,
+            Func<string> getFailMsg = null,
+            IList<Action> failureActions = null)
+        {
+            Logger.Trace(
+                "Asserting that function should be true within {0} seconds at {1}", within.TotalSeconds, DateTime.UtcNow);
+
+            var limit = DateTime.Now.Add(within);
+            do
+            {
+                if (GetResult(function))
+                {
+                    return;
+                }
+                failureActions = failureActions ?? new Action[0];
+                failureActions.ToList().ForEach(x => x());
+                Thread.Sleep((int)(interval == null ? 20 : interval.Value.TotalMilliseconds));
+            } while (limit > DateTime.Now);
+
+            string failMsg = getFailMsg != null ? TryGet(getFailMsg) : function.ToString();
+            Assert.True(false,$"Expected function to be true within {within.TotalSeconds} seconds but it wasn't at {DateTime.UtcNow} [{failMsg}]");
+        }
+
+        private static string TryGet(Func<string> getFailMsg)
+        {
+            try
+            {
+                return getFailMsg();
+            }
+            catch
+            {
+                return "error getting error message";
+            }
+        }
+
+        private static bool GetResult(Func<bool> function)
+        {
+            try
+            {
+                return function();
+            }
+            catch (Exception e)
+            {
+                Logger.Error("While testing for assert the exception was thrown {0}", e.Message);
+                return false;
+            }
+        }
+
+        public static void TrueWithin(Func<bool> function, Action callbackTest, TimeSpan within, TimeSpan? interval = null)
+        {
+            var callbackList = new List<Action> { callbackTest };
+            TrueWithin(function, callbackList, within, interval);
+        }
+
+        public static void TrueWithin(Func<bool> function, List<Action> callbackTests, TimeSpan within, TimeSpan? interval = null)
+        {
+            var limit = DateTime.Now.Add(within);
+            while (limit > DateTime.Now)
+            {
+                Thread.Sleep((int)(interval == null ? 25 : interval.Value.TotalMilliseconds));
+                if (function())
+                {
+                    callbackTests.ForEach(t => t());
+                    return;
+                }
+            }
+        }
+
+        public static void RemainsFalseFor(Func<bool> function, TimeSpan timespan)
+        {
+            var limit = DateTime.Now.Add(timespan);
+            while (limit > DateTime.Now)
+            {
+                if (function())
+                {
+                    Assert.True(false,$"Expected function to remain false for {timespan.TotalSeconds} seconds but it wasn't");
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        public static void RemainsTrueFor(Func<bool> function, TimeSpan timespan)
+        {
+            var limit = DateTime.Now.Add(timespan);
+            while (limit > DateTime.Now)
+            {
+                if (!function())
+                {
+                    Assert.True(false,$"Expected function to remain false for {timespan.TotalSeconds} seconds but it wasn't");
+                }
+                Thread.Sleep(50);
+            }
+        }
+
+        public static void TrueBetween(Func<bool> function, TimeSpan lower, TimeSpan upper)
+        {
+            var start = DateTime.Now;
+            var upperLimit = DateTime.Now.Add(upper);
+            var lowerLimit = DateTime.Now.Add(lower);
+            while (upperLimit > DateTime.Now)
+            {
+                if (function())
+                {
+                    if (DateTime.Now < lowerLimit)
+                    {
+                        Assert.True(false,$"Expected function to remain false for {lower.TotalSeconds} seconds, but was true after {DateTime.Now.Subtract(start).TotalMilliseconds} milliseconds");
+                    }
+                    return;
+                }
+                Thread.Sleep(25);
+            }
+            Assert.True(false,$"Expected function to be true before {upper.TotalSeconds} seconds but it wasn't");
+        }
+
+    }
+
+    public class NullAsyncAssertLogger : IAsyncAssertLogger
+    {
+        public void Trace(string msg, params object[] @params)
+        {
+        }
+
+        public void Error(string msg, params object[] @params)
+        {
+        }
+    }
+}
---
+++ src/AsyncAssert.Xunit/IAsyncAssertLogger.cs
@@ -0,0 +1,9 @@
+﻿namespace AsyncAssert.Xunit
+{
+    public interface IAsyncAssertLogger
+    {
+        void Trace(string msg, params object[] @params);
+
+        void Error(string msg, params object[] @params);
+    }
+}
